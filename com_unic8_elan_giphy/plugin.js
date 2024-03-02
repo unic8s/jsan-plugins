dc6de7ae9c185a53c750f843dd2dd58a47f32a30
unic8s <erik.lembke@unic8.com>
2024-03-02 15:39:46 +0100

    Added giphy plugin

---
+++ com_unic8_elan_giphy/plugin.js
@@ -0,0 +1,86 @@
+module.exports = {
+    options: null,
+    apiKey: '1IIOEdd2okuoCnzL9c93LJSqSpolOuh5',
+    inputTimeout: null,
+    search: "",
+    index: 0,
+    jsonData: null,
+
+    install: function (options) {
+        this.options = options;
+    },
+    input: function (id, data) {
+        switch (id) {
+            case "search":
+                this.search = data;
+
+                if(this.inputTimeout){
+                    clearTimeout(this.inputTimeout);
+                }
+
+                this.inputTimeout = setTimeout(() => {
+                    this.resolveData();
+                }, 1000);
+                break;
+            case "index":
+                if(data < 0){
+                    data = 0;
+                }
+
+                this.index = data;
+
+                this.buildData();
+                break;
+        }
+    },
+
+    startPolling: function () {
+        this.stopPolling();
+
+        this.intervalID = setInterval(() => {
+            this.resolveData();
+        }, this.intervalTime);
+
+        this.resolveData();
+    },
+    stopPolling: function () {
+        clearInterval(this.intervalID);
+    },
+    resolveData: async function () {
+        const url = [
+            'https://api.giphy.com/v1/gifs/search?api_key=',
+            this.apiKey,
+            '&q=',
+            this.search
+        ].join('');
+
+        try {
+            const response = await fetch(url);
+            this.jsonData = (await response.json()).data;
+
+            this.buildData();
+        } catch (ex) { }
+    },
+    buildData: function () {
+        if (!this.jsonData) {
+            return;
+        }
+
+        if(this.index > this.jsonData.length - 1){
+            this.index = this.jsonData.length - 1;
+        }
+
+        let item = this.jsonData[this.index];
+
+        console.log(item);
+
+        if(!item){
+            return;
+        }
+
+        this.options.nodes.outputs.query("image").data = {
+            fileID: item.images.preview_gif.url,
+            path: item.images.preview_gif.url
+        };
+    }
+}
