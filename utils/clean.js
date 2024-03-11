09c69c2ba82b258767980824e22b93780c70ca73
Erik <erik.lembke@unic8.com>
2024-03-11 16:20:14 +0100

    Update clean.js

--- utils/clean.js
+++ utils/clean.js
@@ -6,3 +6,9 @@ const outPath = path.join(__dirname, '../out');
 if (fs.existsSync(outPath)) {
     fs.rmSync(outPath, { recursive: true, force: true });
 }
+
+const listPath = path.join(__dirname, '../plugins.json');
+
+if (fs.existsSync(listPath)) {
+    fs.rmSync(listPath);
+}
