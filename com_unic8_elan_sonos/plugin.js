c660353eaa031b90c4ae00b3fa1889e5c81a607e
unic8s <erik.lembke@unic8.com>
2024-04-04 08:38:35 +0200

    Added controls to sonos plugin

--- com_unic8_elan_sonos/plugin.js
+++ com_unic8_elan_sonos/plugin.js
@@ -4,6 +4,7 @@ module.exports = {
     intervalId: null,
     previousCover: null,
     killed: false,
+    host: null,
 
     install: function (options) {
         this.options = options;
@@ -13,29 +14,47 @@ module.exports = {
 
         this.killed = true;
     },
+    input(id, data) {
+        switch (id) {
+            case "next":
+                this.options.SonosHelper.next(this.host);
+                break;
+            case "previous":
+                this.options.SonosHelper.previous(this.host);
+                break;
+            case "play":
+                this.options.SonosHelper.play(this.host);
+                break;
+            case "pause":
+                this.options.SonosHelper.pause(this.host);
+                break;
+        }
+    },
+    webhook(event) {
+        this.input(event.data);
+    },
 
     stopPolling: function () {
         clearInterval(this.intervalID);
     },
 
     sonos: function (event) {
-        if(this.killed){
+        if (this.killed) {
             return;
         }
 
         switch (event.data.cmd) {
             case "deviceDiscovery":
-                const host = event.data.host;
-                const refThis = this;
+                this.host = event.data.host;
+
+                var refThis = this;
 
                 this.intervalId = setInterval(() => {
-                    refThis.options.SonosHelper.currentTrack(host);
+                    refThis.options.SonosHelper.currentTrack(this.host);
                 }, 2000);
                 break;
             case "currentTrack":
-                const track = event.data.track;
-
-                //console.log(track);
+                var track = event.data.track;
 
                 this.options.nodes.outputs.query("song").data = track.title;
                 this.options.nodes.outputs.query("artist").data = track.artist;
