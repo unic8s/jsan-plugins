e630ec7c4a74c10af5691e84575bcccb8c667b6d
unic8s <erik.lembke@unic8.com>
2024-07-14 21:20:56 +0200

    Added keyboard hooks to control plugin

--- com_unic8_elan_control/plugin.js
+++ com_unic8_elan_control/plugin.js
@@ -3,11 +3,51 @@ module.exports = {
 
     install: function (options) {
         this.options = options;
+
+        window.addEventListener("keyup", this.onKey.bind(this));
+    },
+    uninstall: function () {
+        window.removeEventListener("keyup", this.onKey);
     },
     webhook(event) {
         const node = this.options.nodes.outputs.query(event.data);
 
-        if(node){
+        if (node) {
+            node.data = !node.data;
+        }
+    },
+
+    onKey(event) {
+        let type = null;
+
+        console.log(event);
+
+        switch (event.code) {
+            case "ArrowLeft":
+                type = "left";
+                break;
+            case "ArrowUp":
+                type = "up";
+                break;
+            case "ArrowRight":
+                type = "right";
+                break;
+            case "ArrowDown":
+                type = "down";
+                break;
+            case "Escape":
+                type = "back";
+                break;
+            case "Enter":
+                type = "enter";
+                break;
+            default:
+                return;
+        }
+
+        const node = this.options.nodes.outputs.query(type);
+
+        if (node) {
             node.data = !node.data;
         }
     }
