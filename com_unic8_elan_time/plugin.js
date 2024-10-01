94fea2facd222a412033ffbf4aeb95bc5ae922af
unic8s <erik.lembke@unic8.com>
2024-10-01 23:55:56 +0200

    Improved time plugin

--- com_unic8_elan_time/plugin.js
+++ com_unic8_elan_time/plugin.js
@@ -1,54 +1,29 @@
 module.exports = {
     options: null,
     timestamp: -1,
-    intervalId: null,
+    lastUpdate: 0,
 
     install: function (options) {
         this.options = options;
-
-        this.startTimer();
-    },
-    uninstall: function() {
-        this.stopTimer();
     },
     input: function (id, data) {
-        this.stopTimer();
-
         switch (id) {
             case "timestamp":
                 this.timestamp = data;
                 break;
         }
-
-        if(this.timestamp == -1){
-            this.startTimer();
-        }else{
-            this.calculateTime();
-        }
     },
-    
-    startTimer: function(){
-        this.stopTimer();
-
-        const refThis = this;
-
-        this.intervalId = setInterval(() => {
-            refThis.calculateTime();
-        }, 1000);
-    },
-    stopTimer: function(){
-        if(this.intervalId){
-            clearInterval(this.intervalId);
-
-            this.intervalId = null;
-        }
-    },
-    calculateTime: function () {
+    render: function() {
         const now = this.timestamp >= 0 ? new Date(this.timestamp * 1000) : new Date();
+        const time = now.getTime();
 
-        this.options.nodes.outputs.query("timestamp").data = Math.round(now.getTime() / 1000);
-        this.options.nodes.outputs.query("hours").data = now.getHours();
-        this.options.nodes.outputs.query("minutes").data = now.getMinutes();
-        this.options.nodes.outputs.query("seconds").data = now.getSeconds();
+        if(Math.abs(time - this.lastUpdate) > 1000) {
+            this.options.nodes.outputs.query("timestamp").data = Math.round(time / 1000);
+            this.options.nodes.outputs.query("hours").data = now.getHours();
+            this.options.nodes.outputs.query("minutes").data = now.getMinutes();
+            this.options.nodes.outputs.query("seconds").data = now.getSeconds();
+
+            this.lastUpdate = now;
+        }
     }
 }
