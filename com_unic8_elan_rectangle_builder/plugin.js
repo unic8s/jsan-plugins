f8d93a05a60e5eb283d6178e353981b8da940e65
Erik <erik.lembke@unic8.com>
2024-10-17 15:07:45 +0200

    Update plugin.js

--- com_unic8_elan_rectangle_builder/plugin.js
+++ com_unic8_elan_rectangle_builder/plugin.js
@@ -1,20 +1,18 @@
 module.exports = {
     options: null,
-    outputs: null,
     x: 0,
     y: 0,
     width: 0,
     height: 0,
 
-    install: function (options, inputs, outputs) {
+    install: function (options) {
         this.options = options;
-        this.outputs = outputs;
 
-        this.outputs.bounds = new window.u8.mate.geom.Rectangle(0, 0, 0, 0);
+        this.options.outputs.bounds = new window.u8.mate.geom.Rectangle(0, 0, 0, 0);
     },
     input: function (id, data) {
         this[id] = data;
 
-        this.outputs.bounds = new window.u8.mate.geom.Rectangle(this.x, this.y, this.width, this.height);
+        this.options.outputs.bounds = new window.u8.mate.geom.Rectangle(this.x, this.y, this.width, this.height);
     }
 }
