c15ca98e21463e9f0585922018c3b9499aea0dcf
Erik <erik.lembke@unic8.com>
2024-10-17 15:07:14 +0200

    Update plugin.js

--- com_unic8_elan_random/plugin.js
+++ com_unic8_elan_random/plugin.js
@@ -1,12 +1,10 @@
 module.exports = {
     options: null,
-    outputs: null,
     min: 0,
     max: 0,
 
-    install: function (options, inputs, outputs) {
+    install: function (options) {
         this.options = options;
-        this.outputs = outputs;
     },
     input: function (id, data) {
         switch (id) {
@@ -17,7 +15,7 @@ module.exports = {
                 this.max = data;
                 break;
             case "Trigger":
-                this.outputs.Random = Math.random() * (this.max - this.min) + this.min;
+                this.options.outputs.Random = Math.random() * (this.max - this.min) + this.min;
                 break;
         }
     }
