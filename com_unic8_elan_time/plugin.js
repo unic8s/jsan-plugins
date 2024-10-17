972fb8740075aa6d487bfd51d73578f6b4336189
Erik <erik.lembke@unic8.com>
2024-10-17 15:20:51 +0200

    Update plugin.js

--- com_unic8_elan_time/plugin.js
+++ com_unic8_elan_time/plugin.js
@@ -1,12 +1,10 @@
 module.exports = {
     options: null,
-    outputs: null,
     timestamp: -1,
     lastUpdate: 0,
 
-    install: function (options, inputs, outputs) {
+    install: function (options) {
         this.options = options;
-        this.outputs = outputs;
     },
     input: function (id, data) {
         switch (id) {
@@ -18,6 +16,7 @@ module.exports = {
     render: function() {
         const now = this.timestamp >= 0 ? new Date(this.timestamp * 1000) : new Date();
         const time = now.getTime();
+        const outputs = this.options.outputs;
 
         if(Math.abs(time - this.lastUpdate) > 1000) {
             this.outputs.timestamp = Math.round(time / 1000);
