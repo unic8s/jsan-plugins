6fdb90e6a9d94bc3d10d8e784001cdebfb7c2205
unic8s <erik.lembke@unic8.com>
2024-11-03 09:39:15 +0100

    Added async option to timer plugin

--- com_unic8_elan_timer/plugin.js
+++ com_unic8_elan_timer/plugin.js
@@ -5,13 +5,16 @@ module.exports = {
     delay: 1000,
     active: false,
     toggle: false,
-    timeoutID: null,
+    intervalID: null,
     lastTime: 0,
+    async: false,
 
     install: function (options) {
         this.options = options;
     },
     input: function (id, data) {
+        this.stopTimer();
+
         switch (id) {
             case "Cycles":
                 this.cycles = data;
@@ -22,13 +25,20 @@ module.exports = {
             case "Active":
                 this.active = data;
                 break;
+            case "Async":
+                this.async = data;
+                break;
         }
 
         this.counter = 0;
         this.lastTime = this.getTime();
+
+        if(this.async){
+            this.startTimer();
+        }
     },
     render: function () {
-        if (this.active && (this.cycles == 0 || this.counter < this.cycles)) {
+        if (!this.async && this.active && (this.cycles == 0 || this.counter < this.cycles)) {
             const now = this.getTime();
             const increment = this.delay >= 1 ? this.delay : 1;
 
@@ -48,5 +58,24 @@ module.exports = {
         this.toggle = !this.toggle;
 
         this.options.outputs.Toggle = this.toggle;
+
+        if(this.async && (this.cycles > 0 && this.counter == this.cycles)){
+            this.stopTimer();
+        }
+    },
+    startTimer: function() {
+        this.stopTimer();
+
+        const refThis = this;
+
+        this.intervalID = setInterval(() => {
+            refThis.tick();
+        }, this.delay);
+    },
+    stopTimer: function() {
+        if(this.intervalID){
+            clearInterval(this.intervalID);
+            this.intervalID = null;
+        }
     }
 }
